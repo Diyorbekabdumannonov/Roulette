28c70ab355f1de4d3a67d72dbaf6db2d4a606784
Diyorbek <diyorbekabdumannonov4@gmail.com>
2023-06-08 08:46:15 +0500

    init

---
+++ index.js
@@ -0,0 +1,128 @@
+var options = ["Lose", "$1000000", "Lose", "$350", "$5", "$99"];
+
+var startAngle = 0;
+var arc = Math.PI / (options.length / 2);
+var spinTimeout = null;
+
+var spinArcStart = 10;
+var spinTime = 0;
+var spinTimeTotal = 0;
+
+var ctx;
+
+document.getElementById("spin").addEventListener("click", spin);
+
+function getColor(item) {
+  if (item % 2) {
+    return "#44201f";
+  } else {
+    return "#fff";
+  }
+}
+
+function getFontColor(item) {
+  if (item % 2) {
+    return "#fff";
+  } else {
+    return "#44201f";
+  }
+}
+
+function drawRouletteWheel() {
+  var canvas = document.getElementById("canvas");
+  if (canvas.getContext) {
+    var outsideRadius = 200;
+    var textRadius = 160;
+    var insideRadius = 0;
+
+    ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.lineWidth = 4;
+    for (var i = 0; i < options.length; i++) {
+      var angle = startAngle + i * arc;
+      //ctx.fillStyle = colors[i];
+      ctx.fillStyle = getColor(i);
+
+      ctx.beginPath();
+      ctx.arc(250, 250, outsideRadius, angle, angle + arc, false);
+      ctx.arc(250, 250, insideRadius, angle + arc, angle, true);
+      ctx.stroke();
+      ctx.fill();
+
+      ctx.save();
+      ctx.shadowOffsetX = -1;
+      ctx.shadowOffsetY = -1;
+      ctx.shadowBlur = 0;
+      ctx.shadowColor = "rgb(220,220,220)";
+      ctx.fillStyle = "black";
+      ctx.translate(
+        250 + Math.cos(angle + arc / 2) * textRadius,
+        250 + Math.sin(angle + arc / 2) * textRadius
+      );
+      ctx.rotate(angle + arc / 2 + Math.PI / 2);
+      var text = options[i];
+
+      ctx.font = "16px Brown";
+      ctx.lineWidth = 1;
+      ctx.strokeStyle = getFontColor(i);
+      ctx.fillStyle = getFontColor(i);
+      ctx.strokeText(text, -ctx.measureText(text).width / 2, 0);
+
+      ctx.restore();
+    }
+
+    //Arrow
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    ctx.moveTo(250 - 4, 250 - (outsideRadius + 5));
+    ctx.lineTo(250 + 4, 250 - (outsideRadius + 5));
+    ctx.lineTo(250 + 4, 250 - (outsideRadius - 5));
+    ctx.lineTo(250 + 9, 250 - (outsideRadius - 5));
+    ctx.lineTo(250 + 0, 250 - (outsideRadius - 13));
+    ctx.lineTo(250 - 9, 250 - (outsideRadius - 5));
+    ctx.lineTo(250 - 4, 250 - (outsideRadius - 5));
+    ctx.lineTo(250 - 4, 250 - (outsideRadius + 5));
+    ctx.fill();
+  }
+}
+
+function spin() {
+  spinAngleStart = Math.random() * 10 + 10;
+  spinTime = 0;
+  spinTimeTotal = Math.random() * 3 + 4 * 1000;
+  rotateWheel();
+}
+
+function rotateWheel() {
+  spinTime += 30;
+  if (spinTime >= spinTimeTotal) {
+    stopRotateWheel();
+    return;
+  }
+  var spinAngle =
+    spinAngleStart - easeOut(spinTime, 0, spinAngleStart, spinTimeTotal);
+  startAngle += (spinAngle * Math.PI) / 180;
+  drawRouletteWheel();
+  spinTimeout = setTimeout("rotateWheel()", 30);
+}
+
+function stopRotateWheel() {
+  clearTimeout(spinTimeout);
+  var degrees = (startAngle * 180) / Math.PI + 90;
+  var arcd = (arc * 180) / Math.PI;
+  var index = Math.floor((360 - (degrees % 360)) / arcd);
+  ctx.save();
+  ctx.font = "bold 30px Helvetica, Arial";
+  var text = options[index];
+  alert(text);
+  ctx.restore();
+}
+
+function easeOut(t, b, c, d) {
+  var ts = (t /= d) * t;
+  var tc = ts * t;
+  return b + c * (tc + -3 * ts + 3 * t);
+}
+
+drawRouletteWheel();
