22a63ae33a359418301e500b6ab5621aa0d06939
Diyorbek <diyorbekabdumannonov4@gmail.com>
2023-06-08 17:44:40 +0500

    updated

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var options = [
+// CUSTOM OPTIONS
+let options = [
   "Lose",
   "$1000000",
   "Lose",
@@ -9,131 +10,130 @@ var options = [
   " 1 Glace",
 ];
 
-var startAngle = 0;
-var arc = Math.PI / (options.length / 2);
-var spinTimeout = null;
+const colors = [
+  ["#fff", "#44201f"],
+  ["#44201f", "#fff"],
+  ["black", "#fff"],
+];
 
-var spinArcStart = 10;
-var spinTime = 0;
-var spinTimeTotal = 0;
+let startAngle = 0;
+let arc = Math.PI / (options.length / 2);
+let spinTimeout = null;
 
-var ctx;
+let spinArcStart = 10;
+let spinTime = 0;
+let spinTimeTotal = 0;
 
-document.getElementById("spin").addEventListener("click", spin);
+let ctx;
+const spinBtn = document.getElementById("spin");
+const marker = document.querySelector(".marker");
 
-function getColor(item) {
-  if (item % 2) {
-    return "#fff";
-  } else {
-    return "#44201f";
-  }
-}
+spinBtn.addEventListener("click", spin);
 
-function getFontColor(item) {
-  if (item % 2) {
-    return "#44201f";
-  } else {
-    return "#fff";
-  }
-}
+// Drawing Wheel
 
 function drawRouletteWheel() {
-  var canvas = document.getElementById("canvas");
+  const canvas = document.getElementById("canvas");
+  window.devicePixelRatio = 2;
+  const scale = window.devicePixelRatio;
+  let size = 250;
+  canvas.width = Math.floor(size * scale);
+  canvas.height = Math.floor(size * scale);
+
   if (canvas.getContext) {
-    var outsideRadius = 200;
-    var textRadius = 160;
-    var insideRadius = 0;
+    const outsideRadius = 200;
+    const textRadius = 160;
+    const insideRadius = 0;
 
     ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     for (var i = 0; i < options.length; i++) {
-      var angle = startAngle + i * arc;
-      ctx.fillStyle = getColor(i);
+      const angle = startAngle + i * arc;
+      const currentColor = i % colors.length;
+      ctx.fillStyle = colors[currentColor][0];
       ctx.strokeStyle = "#44201f";
       ctx.lineWidth = 6;
       ctx.beginPath();
       ctx.arc(250, 250, outsideRadius, angle, angle + arc, false);
       ctx.arc(250, 250, insideRadius, angle + arc, angle, true);
-      ctx.stroke();
       ctx.fill();
       ctx.save();
-      ctx.shadowOffsetX = -1;
-      ctx.shadowOffsetY = -1;
-      ctx.shadowBlur = 0;
-      ctx.shadowColor = "rgb(220,220,220)";
-      ctx.fillStyle = "black";
       ctx.translate(
         255 + Math.cos(angle + arc / 2) * textRadius,
         255 + Math.sin(angle + arc / 2) * textRadius
       );
       ctx.rotate(angle + arc / 2 - 0.1);
-      var text = options[i];
+      const text = options[i];
 
-      ctx.font = "20px sans-serif";
+      ctx.font = "20px custom";
+      ctx.scale(scale / 1.5, scale / 1.5);
       ctx.lineWidth = 1;
       ctx.textAlign = "center";
-      ctx.strokeStyle = getFontColor(i);
-      ctx.fillStyle = getFontColor(i);
+      ctx.strokeStyle = colors[currentColor][1];
+      ctx.fillStyle = colors[currentColor][1];
       ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
-
       ctx.restore();
     }
   }
 }
 
+// Spin the wheel
+
 function spin() {
   spinAngleStart = Math.random() * 10 + 10;
   spinTime = 0;
   spinTimeTotal = Math.random() * 3 + 4 * 1000;
+  spinBtn.disabled = true;
   rotateWheel();
 }
 
+// Rotate wheel
 function rotateWheel() {
-  spinTime += 30;
+  spinTime += 7;
   if (spinTime >= spinTimeTotal) {
     stopRotateWheel();
     return;
   }
-  var spinAngle =
+  const spinAngle =
     spinAngleStart - easeOut(spinTime, 0, spinAngleStart, spinTimeTotal);
   startAngle += (spinAngle * Math.PI) / 180;
+  const degrees = ((startAngle * 180) / Math.PI) % 360;
 
-  const marker = document.querySelector(".arrow");
-
-  var degrees = (startAngle * 180) / Math.PI;
-  var arcd = (arc * 180) / Math.PI;
-  var index = Math.floor((360 - (degrees % 360)) / arcd);
-
-  [1, 2, 3, 4, 5, 6, 7, 8, 9].map((index) => {
+  options.map((el, index) => {
     const avrg = 360 / options.length;
 
-    if (Math.abs(avrg * (index + 1) - Math.round(degrees)) < 10) {
+    console.log(degrees);
+
+    if (avrg * (index + 1) == Math.round(degrees)) {
       marker.classList.add("bounce");
       setTimeout(() => {
         marker.classList.remove("bounce");
-      }, 300);
+      }, 100);
     }
   });
 
   drawRouletteWheel();
-  spinTimeout = setTimeout("rotateWheel()", 90);
+  spinTimeout = setTimeout("rotateWheel()", 10);
 }
 
+// Stop Wheel
 function stopRotateWheel() {
   clearTimeout(spinTimeout);
-  var degrees = (startAngle * 180) / Math.PI;
-  var arcd = (arc * 180) / Math.PI;
-  var index = Math.floor((360 - (degrees % 360)) / arcd);
+  const degrees = (startAngle * 180) / Math.PI;
+  const arcd = (arc * 180) / Math.PI;
+  const index = Math.floor((360 - (degrees % 360)) / arcd);
   ctx.save();
-  var text = options[index];
+  const text = options[index];
   alert(text);
+  spinBtn.disabled = false;
   ctx.restore();
 }
 
+// Animation
 function easeOut(t, b, c, d) {
-  var ts = (t /= d) * t;
-  var tc = ts * t;
+  const ts = (t /= d) * t;
+  const tc = ts * t;
   return b + c * (tc + -3 * ts + 3 * t);
 }
 
